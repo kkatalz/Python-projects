a857c51e453d3aedff97ef090b30d41cd6155c24
kkatalz <zlata.kiev2004@gmail.com>
2024-12-07 15:09:37 +0200

    Change questions' view to dictionary

--- practice_tasks/quizGame.py
+++ practice_tasks/quizGame.py
@@ -1,11 +1,14 @@
 from colorama import Fore, Style
 
 
-def question(question, firstOption, secOption, thOption, fourthOption, correctAnswer):    
+def quizGame(question, firstOption, secOption, thOption, fourthOption, correctAnswer):    
 
     print("\n" + question)
-    print(f"\nA. {firstOption} \nB. {secOption} \nC. {thOption} \nD. {fourthOption}")
+    print(f"\n{firstOption}\n{secOption}\n{thOption}\n{fourthOption}")
     userAnswer = input("Your answer: ").lower()
+    for i in range(len(correctAnswer)):
+        correctAnswer[i] = correctAnswer[i].lower()
+
     if userAnswer in correctAnswer:
         print(f"{Fore.GREEN}Correct!{Style.RESET_ALL}")
         return 1
@@ -15,43 +18,42 @@ def question(question, firstOption, secOption, thOption, fourthOption, correctAn
                 correctAnswers = correctAnswer[i].upper()
             else:
                 correctAnswers += " or " + correctAnswer[i].upper()
-                
+
         print(f"{Fore.RED} Wrong. The correct answers are {correctAnswers} {Style.RESET_ALL}")
         return 0
     
 
 def main():
-    firstQuestion = "Question 1: What is the capital of France?"
-    firstQuestionOptions = {
-        "A": "Berlin",
-        "B": "Madrid",
-        "C": "Paris",
-        "D": "Rome"
-    }
-    firstQuestionCorrectAnswer = ["c", "paris"]
-
-    secondQuestion = "Question 2: What planet is known as the Red Planet?"
-    secondQuestionOptions = {
-        "A": "Earth",
-        "B": "Jupiter",
-        "C": "Mars",
-        "D": "Venus"
-    }
-    secondQuestionCorrectAnswer = ["c", "mars"]
-
-    thirdQuestion = "Question 3: What is the largest ocean in the world?"
-    thirdQuestionOptions = {
-        "A": "Atlantic Ocean",
-        "B": "Indian Ocean",
-        "C": "Pacific Ocean",
-        "D": "Northern Ocean"
-    }
-    thirdQuestionCorrectAnswer = ["c", "pacific ocean"]
+    quiz = [
+        {
+            "question": "What is the capital of France?",
+            "options": ["A. Berlin", "B. Madrid", "C. Paris", "D. Rome"],
+            "correctAnswer": ["C", "Paris"]
+        },
+    
+        {
+            "question": "What planet is known as the Red Planet?",
+            "options": ["A. Earth", "B. Jupiter", "C. Mars", "D. Venus"],
+            "correctAnswer": ["C", "Mars"]
+        },
+    
+        {
+            "question": "What is the largest ocean in the world?",
+            "options": ["A. Atlantic Ocean", "B. Indian Ocean", "C. Pacific Ocean", "D. Northern Ocean"],
+            "correctAnswer": ["C", "Pacific Ocean"]
+        }
+    ]
 
     rightAnswers = 0
-    rightAnswers += question(firstQuestion, firstQuestionOptions["A"], firstQuestionOptions["B"], firstQuestionOptions["C"], firstQuestionOptions["D"], firstQuestionCorrectAnswer)
-    rightAnswers += question(secondQuestion, secondQuestionOptions["A"], secondQuestionOptions["B"], secondQuestionOptions["C"], secondQuestionOptions["D"], secondQuestionCorrectAnswer)
-    rightAnswers += question(thirdQuestion, thirdQuestionOptions["A"], thirdQuestionOptions["B"], thirdQuestionOptions["C"], thirdQuestionOptions["D"], thirdQuestionCorrectAnswer)
+    for index, question in enumerate(quiz,1):
+        quizQuestion = "Question " + str(index) + ". " + question["question"]
+        option1 = question["options"][0]
+        option2 = question["options"][1]
+        option3 = question["options"][2]
+        option4 = question["options"][3]
+        correctAnswer = question["correctAnswer"]
+
+        rightAnswers += quizGame(quizQuestion, option1, option2, option3, option4, correctAnswer)
 
     print(f"\nQuiz over! You got {rightAnswers} out of 3 questions right.")
 
