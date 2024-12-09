8dc7b3c669d93bf2fd752fa4a0a84875a6abb05d
kkatalz <zlata.kiev2004@gmail.com>
2024-12-09 17:13:25 +0200

    Refactor tic tac toe game logic to use constants for rows and players, and improve game over conditions

--- practice_tasks/ticTacToeGame.py
+++ practice_tasks/ticTacToeGame.py
@@ -1,19 +1,26 @@
+ROW0 = "row0"
+ROW1 = "row1"
+ROW2 = "row2"
+firstPlayer = "X"
+secPlayer = "Y"
+gameIsOver = False
+
 def gameField():
     field = {
         "separator": "---+---+---",
-        "row0": "   |   |   ",
-        "row1": "   |   |   ",
-        "row2": "   |   |   "
+        ROW0: "   |   |   ",
+        ROW1: "   |   |   ",
+        ROW2: "   |   |   "
     }
     return field
 
 def fieldLook(field):
     print("\n"+ field["separator"])
-    print(field["row0"])
+    print(field[ROW0])
     print(field["separator"])
-    print(field["row1"])
+    print(field[ROW1])
     print(field["separator"])
-    print(field["row2"])
+    print(field[ROW2])
     print(field["separator"] +"\n")
 
 def fillField(field, player):
@@ -44,49 +51,72 @@ def fillField(field, player):
             break
 
         if row == 0:
-            row = "row0"
+            row = ROW0
         elif row == 1:
-            row = "row1"
+            row = ROW1
         elif row == 2:
-            row = "row2"
+            row = ROW2
 
         row_parts = field[row].split("|")
+        firstColumn_part = (field[ROW0].split("|")[0] + field[ROW1].split("|")[0] + field[ROW2].split("|")[0]).replace(" ", "")
+        secColumn_part = (field[ROW0].split("|")[1] + field[ROW1].split("|")[1] + field[ROW2].split("|")[1]).replace(" ", "")
+        thirdColumn_part = (field[ROW0].split("|")[2] + field[ROW1].split("|")[2] + field[ROW2].split("|")[2]).replace(" ", "")
+        # print("First column: ", firstColumn_part)
 
         if row_parts[column].strip() == "":  
             row_parts[column] = f" {player} "
-            
             break
 
         elif row_parts[column].strip() == "X" or row_parts[column].strip() == "Y":
             print("That spot is already taken. Try again!")
             continue  
-        # check if all spots are taken
+
         elif all([x.strip() != "" for x in row_parts]):
             print("All spots are taken. Game over!")
+            gameIsOver = True
+            
+        
+        elif all([x.strip() == firstPlayer for x in row_parts]) or all(el == firstPlayer for el in firstColumn_part) or all(el == firstPlayer for el in secColumn_part) or all(el == firstPlayer for el in thirdColumn_part):
+            print(f"Player {firstPlayer} won!")
+            gameIsOver = True
+            
+        elif all([y.strip() ==secPlayer for y in row_parts]) or all(el == secPlayer for el in firstColumn_part) or all(el == secPlayer for el in secColumn_part) or all(el == secPlayer for el in thirdColumn_part):
+            print(f"Player {secPlayer} won!")
+            gameIsOver = True
+            
+        
+        else:
+            print("That's a tie!")
+            gameIsOver = True
+            
+        if gameIsOver:
             break
+            
+
+
 
     field[row] = "|".join(row_parts)
     fieldLook(field)
 
 def main():
-    firstPlayer = "X"
-    secPlayer = "Y"
 
     print("Welcome to Tic Tac Toe!")
     field = gameField()
-
     fieldLook(field)
 
+    # do{}
 
-    print(f"Player {firstPlayer}'s turn")
-    fillField(field, firstPlayer)
 
-    print(f"Player {secPlayer}'s turn")
-    fillField(field, secPlayer)
+    while True:
+    
+        print(f"Player {firstPlayer}'s turn")
+        
+        if not fillField(field, firstPlayer):
+            continue 
 
-# TODO: Add logic to check is someone won the game
-# TODO: Add logic to check if the game is a tie
-# TODO: Add logic to check if the game is over
+        print(f"Player {secPlayer}'s turn")
+        if not fillField(field, secPlayer):
+            continue
 
 
 
