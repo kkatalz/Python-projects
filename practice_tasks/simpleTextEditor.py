ea4e72f724417538f9d2d310445390883cba9993
kkatalz <zlata.kiev2004@gmail.com>
2024-12-10 23:41:15 +0200

    Refactor file handling in simple text editor to use absolute paths and improve error handling

--- practice_tasks/simpleTextEditor.py
+++ practice_tasks/simpleTextEditor.py
@@ -1,23 +1,30 @@
+import os
+
 def main():
+    script_directory = os.path.dirname(os.path.abspath(__file__))
 
     while True:
-        file = input("Enter the file name to open or create (.txt): ")
-        
-        if file.endswith(".txt"):
-            try:
-                with open(file, "r+") as f:
+        file = input("Enter the file name to open or create: ").strip()
+        file_path = os.path.join(script_directory, file)
+
+        try:
+            if os.path.exists(file_path):
+                with open(file_path, "r") as f:
                     print("File opened.")   
                     print(f.read())
-                     
-            except FileNotFoundError:
-                with open(file, "w+") as f: 
+                    
+            else:
+                with open(file_path, "w") as f: 
                     print("File created.")
-            break
-        else:
-            print("Invalid file name.")
+            break 
+
+        except OSError:
+            print("Error opening file.")
+            return
+      
                 
 
-    with open(file, "a") as f:
+    with open(file_path, "a") as f:
         print("\nEnter your text (type SAVE on a new line to save and exit the file):")
         while True:
             text = input()
