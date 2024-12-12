0f2d679bc5f2b2054076b49f61cb257e5f4afb21
kkatalz <zlata.kiev2004@gmail.com>
2024-12-12 17:56:53 +0200

    Modulize, remove magic numbers, simplify logic

--- practice_tasks/pigDiceGame.py
+++ practice_tasks/pigDiceGame.py
@@ -1,41 +1,63 @@
 import random
 from termcolor import colored
+import sys
+print(sys.path)
+
+players = [1, 2]
+first_player = players[0]
+second_player = players[1]
+
+players_scores = {first_player: 0, second_player: 0}
+stop_game_for_player_number = 1
+
+
+def player_score_per_game(player):
+    
+    print(f"\nPlayer {player}'s turn")
+    while True:
+        random_dice_points = random.randint(1, 6)
+        print(f"You rolled a {random_dice_points}")
+
+        if random_dice_points == stop_game_for_player_number:
+            players_scores[player] = 0
+            break
+        
+        else:
+            players_scores[player] += random_dice_points
+            roll_again_answer = input("Roll again? (y/n): ").lower()
+            if roll_again_answer == "y":
+                continue 
+            else: break
+
+    current_player_score = players_scores[player]
+    print(colored(f"\nYou scored {current_player_score} points this turn", 'light_magenta'))
+    return current_player_score
+
+def curent_players_scores(player, current_player_score):
+
+    if player == first_player:
+        next_player = second_player
+        print(colored(f"Current scores: Player {player}: {current_player_score}, {next_player}: {players_scores[next_player]}", 'light_green'))
+    
+    else:
+        previous_player = first_player
+        print(colored(f"Current scores: Player {previous_player}: {players_scores[previous_player]}, {player}: {current_player_score}", 'light_green'))
 
 def main():
-    current_score = 0
-    players = [1, 2]
-    players_scores = {players[0]: 0, players[1]: 0}
     
+    current_player_score = 0
+    winner_score = int(input("\nEnter the points player must get to win: "))
     
-    while current_score != 100:
+    while current_player_score < winner_score:
+
         for player in players:
-            print(f"\nPlayer {player}'s turn")
 
-            while True:
-                random_dice_points = random.randint(1, 6)
-                print(f"You rolled a {random_dice_points}")
+            if current_player_score >= winner_score: break
 
-                if random_dice_points == 1:
-                    players_scores[player] = 0
-                    break
-                
-                else:
-                    players_scores[player] += random_dice_points
-
-                    roll_again_answer = input("Roll again? (y/n): ").lower()
-                    if roll_again_answer == "y":
-                         continue 
-                    else: break
-
-            current_score = players_scores[player]
-            print(colored(f"\nYou scored {current_score} points this turn", 'light_magenta'))
-
-            if str(player) == "1":
-                next_player = 2
-                print(colored(f"Current scores: Player {player}: {current_score}, {next_player}: {players_scores[next_player]}", 'light_green'))
-            else:
-                next_player = 1
-                print(colored(f"Current scores: Player {next_player}: {players_scores[next_player]}, {player}: {current_score}", 'light_green'))
+            current_player_score = player_score_per_game(player)
+            curent_players_scores(player, current_player_score)
+    
+    print(colored(f"Player {player} wins!", 'light_green'))
 
 
 if __name__ == "__main__":
