e8971204460a23f686f69514caccf656665100eb
kkatalz <zlata.kiev2004@gmail.com>
2024-12-19 18:08:31 +0200

    Improve regular expressions logic

--- practice_tasks/passwordStrengthChecker.py
+++ practice_tasks/passwordStrengthChecker.py
@@ -1,9 +1,9 @@
 # This program checks the strength of a password based on the following criteria:
 # Very strong: 8 characters or more, at least one uppercase letter, one lowercase letter, one special character, and digits
 # Strong: 8 characters or more, at least one uppercase letter, one lowercase letter, and digits
-# Medium: 8 characters or more, at least one uppercase letter, and digits
-# Weak: 8 characters or more, and digits
-# Very weak: 7 characters or fewer, and digits
+# Medium: 8 characters or more, at least one uppercase letter or lowercase letter, and digits
+# Weak: 8 characters or more: digits
+# Very weak: 7 characters or fewer: digits
 
 import re
 
@@ -11,11 +11,11 @@ import re
 def check_password_strength(user_password):
     
     strength = 0
-    very_strong_regex = r"(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()-+])(?=.*\d).{8,}"
-    strong_regex = r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}"
-    medium_regex = r"^(?=.*[A-Z])(?=.*\d).{8,}$"
-    weak_regex = r"\d{8,}"
-    very_weak_regex = r"\d{1,7}"
+    very_strong_regex = r"(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()-+])(?=.*\d).{8,}"
+    strong_regex = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$"
+    medium_regex = r"^(?=.*[A-Z])(?=.*\d)|(?=.*[a-z])(?=.*\d).{8,}$"
+    weak_regex = r"^\d{8,}$"
+    very_weak_regex = r"^\d{1,7}&"
  
     if re.match(very_strong_regex, user_password):
         strength = "Very strong"
