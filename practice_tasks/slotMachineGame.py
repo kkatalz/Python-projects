2d3a4dda97baa34c3378f5e641ff6ebc02f128f9
kkatalz <zlata.kiev2004@gmail.com>
2024-12-24 19:38:11 +0200

    Add functions for independent piece of logic

--- practice_tasks/slotMachineGame.py
+++ practice_tasks/slotMachineGame.py
@@ -2,6 +2,7 @@
 # if 3 match then you win 10 times more
 import random
 current_balance = 0
+bet_amount = 0
 
 choice_icons = ("🍉", "🍋", "🍇", "🥑", "🍗")
 
@@ -14,29 +15,28 @@ def choose_icons():
     icons_set = (first_icon, sec_icon, trd_icon)
     return icons_set
 
-def calculate_current_balance(bet_amount, winning_money):
+def calculate_current_balance(winning_money):
     global current_balance
     current_balance += (winning_money - bet_amount)   
     current_balance = round(current_balance,3)
 
-def check_victory(set_icons, bet_amount):
-    global current_balance
+def check_victory(set_icons):
     winning_money = 0    
     first_icon, sec_icon, trd_icon = set_icons
 
     if first_icon == sec_icon == trd_icon:
         winning_money = bet_amount * 10
         print(f"You won {winning_money}!")
-        calculate_current_balance(bet_amount, winning_money)
+        calculate_current_balance(winning_money)
 
 
     elif first_icon == sec_icon or sec_icon == trd_icon or first_icon == trd_icon:
         winning_money = bet_amount * 2
-        print(f"You won {bet_amount * 2}!")
-        calculate_current_balance(bet_amount, winning_money)
+        print(f"You won {winning_money}!")
+        calculate_current_balance(winning_money)
     
     else:
-        calculate_current_balance(bet_amount, winning_money)
+        calculate_current_balance(winning_money)
         print("You lost!")
         if current_balance <=0:
             print("You are out of money! Game is over.")
@@ -44,38 +44,39 @@ def check_victory(set_icons, bet_amount):
     
     return current_balance
 
-def game():
-    global current_balance
-
+def get_bet_amount():
+    global bet_amount
     while True:
-        print(f"\nCurrent ballance: ${current_balance}")
-
-        while True:
             bet_amount = float(input("Enter your bet amount: $").strip())
             try:
                 if bet_amount > current_balance or bet_amount < 1:    
                     print(f"Invalid bet amount. You can bet between $1 and ${current_balance}")   
                 else:
-                    break
+                    return
 
             except ValueError:
                 print("Please enter a valid number for the bet amount")
-            
 
-        set_icons = choose_icons()
-        check_victory(set_icons, bet_amount)
-
-        while True:
+def play_again():
+    while True:
             play_again_answer = input("\nDo you want to play again? (y/n): ").strip().lower()
 
             if play_again_answer in ('y', 'yes'):
-                break
+                return
             elif play_again_answer in ('n', 'no'):
                 print(f"\nYour current balance is {current_balance}. \nGame is over.")
                 exit()
             else:
                 print("Incorrect input")
                 continue
+
+def game():
+    while True:
+        print(f"\nCurrent ballance: ${current_balance}")
+        get_bet_amount()
+        set_icons = choose_icons()
+        check_victory(set_icons)
+        play_again()
 
 def get_staring_balance():
     global current_balance
