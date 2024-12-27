c9f12b2cb03ec4f3f786f5a80659981635a7021e
kkatalz <zlata.kiev2004@gmail.com>
2024-12-27 13:31:41 +0200

    Add function to make a deposit

--- practice_tasks/ATM.py
+++ practice_tasks/ATM.py
@@ -2,7 +2,7 @@ class ATM():
     def __init__(self, balance):
         self.balance = balance
 
-    def showProgramOptions(self):
+    def show_program_options(self):
         options = {
             "1" : "Check Balance",
             "2" : "Deposit",
@@ -15,14 +15,35 @@ class ATM():
 
         
 
-    def showBalance(self):
+    def show_balance(self):
         print(f"Your current balance is: ${self.balance}")
+
+    def make_deposit(self):
+        while True:
+            try:
+                deposit_amount = float(input("\nEnter the amount to deposit: ").strip())
+                if deposit_amount <= 0:
+                    print("Deposit amount must be positive.")
+
+                else:
+                    self.balance = deposit_amount
+                    print(f"Sucessfuly deposited ${deposit_amount}.")
+                    return
+            except ValueError:
+                print("Please enter a valid number.")
+
+
 
 
     def main(self):
+        
         print("\nWelcome to the ATM!")
-        self.showProgramOptions()
-        self.showBalance()
+        self.show_program_options()
+
+        # chosen_option = input("Please choose an option: ").strip()
+        
+        self.make_deposit()
+        print(self.balance)
     
      
     
